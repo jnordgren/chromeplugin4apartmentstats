dcba1b89c309e9b5560ba41840b50c891249f9b0
JakeZorx <jnordgren@gmail.com>
2016-02-19 04:38:07 +0100

    Added support for catching pricing information, can commnuicate it back to the background processes yet though...

--- content.js
+++ content.js
@@ -1,5 +1,25 @@
 
 
+
+// chrome.webRequest.onCompleted.addListener(function(details) {
+//   /* Process the XHR response */
+//   alert("Vi fångade något");
+// }, {urls: ["<all_urls>"]});
+//
+
+
+/*Inject js to catch ajax data. kudos RobW st*/
+/*Source: https://stackoverflow.com/questions/9515704/building-a-chrome-extension-inject-code-in-a-page-using-a-content-script/9517879#9517879*/
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('injection_script.js');
+s.onload = function() {
+    this.parentNode.removeChild(this);
+};
+(document.head || document.documentElement).appendChild(s);
+
+
+
+
 getInfo();
 
 function getInfo(){
@@ -18,6 +38,17 @@ function getInfo(){
         break;
     }
   }
+
+  chrome.runtime.sendMessage({
+      type: 'object_info',
+      request: "closing_price"
+  }, function(data) {
+      // Received message from background, pass to page
+      var event = document.createEvent('Events');
+      event.initEvent(EVENT_REPLY, false, false);
+      transporter.setAttribute('result', JSON.stringify(data));
+      transporter.dispatchEvent(event);
+  });
 
   return info;
 }
