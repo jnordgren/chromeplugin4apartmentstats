f694483861e5fb9aa1d99a0a1ed9436e993929ca
JakeZorx <jnordgren@gmail.com>
2016-02-20 15:46:55 +0100

    Cleaning up

--- injection_script.js
+++ injection_script.js
@@ -20,28 +20,14 @@
           //Here we have found what we were looking for.
           if(this.responseText.length > 4){ //How should we communicate this back to our thing? put it into dom?
             document.getElementById('special_l33t_inject_div').innerHTML = this.responseText;
-            // chrome.runtime.sendMessage({
-            //     type: 'closing_price',
-            //     request: "closing_price"
-            // }, function(data) {
-            //     // Received message from background, pass to page
-            //     var event = document.createEvent('Events');
-            //     event.initEvent(EVENT_REPLY, false, false);
-            //     transporter.setAttribute('result', JSON.stringify(data));
-            //     transporter.dispatchEvent(event);
-            // });
+
           }
             /* Method        */ this._method
             /* URL           */ this._url
             /* Response body */ this.responseText
             /* Request body  */ postData
         });
-        // alert("HEJS" );
-        // console.log("---");
-        // if(this.responseText.length > 4){
-          // console.log(this);
-          // console.log(this.XMLHttpRequest.responseText);
-        // }
+
         return send.apply(this, arguments);
     };
 })();
